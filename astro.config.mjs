a00dfa03441112ca5463532866f1af0650197f13
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-05-25 11:04:32 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ar'],
+    routing: {
+      // false means 'en' paths are root (/) and 'ar' paths are prefixed (/ar/)
+      // prefixDefault: false, 
+      
+      // Redirects root (/) automatically if browser preferences match another locale
+      // fallbackType: 'redirect' 
+      prefixDefaultLocale : true,
+      
+    }
+  }
+});
---
+++ src/components/base/index.ts
@@ -0,0 +1,3 @@
+export { default as LanguageSelector} from './LanguageSelector.astro'
+export { default as ThemeSwitcher } from './ThemeSwitcher.astro'
+export { default as SearchModal } from './SearchModal.astro'
